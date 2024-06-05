4d34a8716018583ae6865c9edc00d66c9127b558
vohn237 <devohn99@outlook.com>
2024-06-05 15:08:28 -0500

    adding routes to page nav links

---
+++ components/MobileNav.jsx
@@ -0,0 +1,12 @@
+'use client';
+
+import Link from 'next/link';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
+import { usePathname } from 'next/navigation';
+import { CiMenuFries } from 'react-icons/ci';
+
+const MobileNav = () => {
+  return <nav>MobileNav</nav>;
+};
+
+export default MobileNav;
---
+++ components/Stairs.jsx
@@ -0,0 +1,38 @@
+import { animate, motion } from 'framer-motion';
+
+const stairAnimation = {
+  initial: { top: '0%' },
+  animate: { top: '100%' },
+  exit: { top: ['100%', '0%'] },
+};
+
+// calculate the reverse index for staggered animation
+const reverseIndex = (index) => {
+  const totalSteps = 6; // total number of steps
+  return totalSteps - index - 1; // reverse index
+};
+const Stairs = () => {
+  return (
+    <>
+      {/* Render 6 motion divs, each representing a step of the stairs. Each div will have the same animation defined by the stairsAnimation object. The delay for each div is calculated dynamically based on it's reversed index, creating a staggered effect with decreasing delay for each subsequent step. */}
+
+      {[...Array(6)].map((_, index) => (
+        <motion.div
+          key={index}
+          variants={stairAnimation}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          transition={{
+            duration: 0.4,
+            ease: 'easeInOut',
+            delay: reverseIndex(index) * 0.1,
+          }}
+          className="h-full w-full bg-white relative"
+        />
+      ))}
+    </>
+  );
+};
+
+export default Stairs;
