f4f4d748ce28fbe0bec35ab9975b3fa1087b90d5
vohn237 <devohn99@outlook.com>
2024-06-05 18:20:36 -0500

    added mobile navigation, an updated styles on desktop button

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -3,10 +3,53 @@
 import Link from 'next/link';
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
 import { usePathname } from 'next/navigation';
-import { CiMenuFries } from 'react-icons/ci';
+import { AiOutlineMenu } from 'react-icons/ai';
+
+const links = [
+  { name: 'home', path: '/' },
+  { name: 'services', path: '/services' },
+  { name: 'resume', path: '/resume' },
+  { name: 'work', path: '/work' },
+  { name: 'contact', path: '/contact' },
+];
 
 const MobileNav = () => {
-  return <nav>MobileNav</nav>;
+  const pathname = usePathname();
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <AiOutlineMenu className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        {/* logo */}
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1 className="mt-32 mb-40 text-center text-2xl">
+              Devohn <span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+
+        {/* nav links */}
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                key={index}
+                href={link.path}
+                className={`${
+                  link.path === pathname &&
+                  'text-accent border-b-2 border-accent'
+                } text-xl capitalize hover:text-accent transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
