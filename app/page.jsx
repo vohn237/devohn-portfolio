0db4fdea6fc79ef206a4381c2301fb715cdd19d2
vohn237 <devohn99@outlook.com>
2024-06-05 21:37:04 -0500

    workign on homepage

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row justify-between items-center xl:pt-8 xl:pb-24">
           {/* Career  */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hey I&#39;m <br />
@@ -41,7 +41,7 @@ const Home = () => {
           </div>
           {/* Photo */}
           <div>
-            <Photo />
+            <Photo className=" order-1 xl:order-none mb-8 xl:mb-0" />
           </div>
         </div>
       </div>
