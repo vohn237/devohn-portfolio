2d3d2d7fca5d6791b5438897c4c4cd7c27b2ee13
vohn237 <devohn99@outlook.com>
2024-06-08 21:21:25 -0500

    color update

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -43,7 +43,7 @@ const Photo = () => {
             cx="253"
             cy="253"
             r="250"
-            stroke="#00ff99"
+            stroke="var(--color-accent)"
             strokeWidth="4"
             strokeLinecap="round"
             strokeLinejoin="round"
