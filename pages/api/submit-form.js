86ddab6b52aced079f62ece79a9020b9c35c9c9b
vohn237 <devohn99@outlook.com>
2024-06-11 08:43:39 -0500

    added mysql database for form submission

--- pages/api/submit-form.js
+++ pages/api/submit-form.js
@@ -1,19 +1,20 @@
-export default function handler(req, res) {
+import db from '../../lib/mysql';
+
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { firstname, lastname, email, phone, service, message } = req.body;
-    // Handle the incoming form data as needed (e.g., store in database, send an email, etc.)
-    console.log(
-      'Received submission:',
-      firstname,
-      lastname,
-      email,
-      phone,
-      service,
-      message
-    );
-    res.status(200).json({ message: 'Form submitted successfully!' });
+    try {
+      const query =
+        'INSERT INTO submissions (firstname, lastname, email, phone, service, message) VALUES (?, ?, ?, ?, ?, ?)';
+      const values = [firstname, lastname, email, phone, service, message];
+      const result = await db.query(query, values);
+      console.log('Inserted data:', result);
+      res.status(200).json({ message: 'Form submitted successfully!' });
+    } catch (error) {
+      console.error('Database error:', error);
+      res.status(500).json({ message: 'Failed to submit form.' });
+    }
   } else {
-    // Handle any other HTTP methods, or send a method not allowed status
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
